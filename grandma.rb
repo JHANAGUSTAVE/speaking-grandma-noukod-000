cee629aa4e33b09f6ecbdcad4c268a16dca053ae
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-15 18:48:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase = 'I LOVE YOU GRANDMA!')
   if phrase == 'I LOVE YOU GRANDMA!'
-    puts 
+    puts "HUH?! SPEAK UP, SONNY"
+  else
 end
 
 # Whatever you say to grandma, she should respond with
